6fe7cf34c854b12a511c355fca9957b9b82d262f
Fabio Jun Takada Chino <fchino@opencs.com.br>
2015-03-12 05:01:57 -0300

    More changes in the Base2NCodec. This implementation is not ready yet.

--- src/main/java/br/com/opencs/bincodec/Base2NCodec.java
+++ src/main/java/br/com/opencs/bincodec/Base2NCodec.java
@@ -2,7 +2,40 @@ package br.com.opencs.bincodec;
 
 import java.io.IOException;
 
+/**
+ *
+ *
+ * <h3>Ignored characters</h3>
+ * 
+ * <p>This class optionally allows the definition of some characters to be ignored from
+ * the input while decoding data. This can be enabled by providing a list of characters
+ * as a char array.</p>
+ * 
+ * <p>Since this list is verified right after the character is extracted from the source,
+ * it is very important to ensure that this list does not contain the padding character
+ * or any other character valid for the specified alphabet.</p>
+ * 
+ * <h3>Padding</h3>
+ * 
+ * <p>This class implements a customized padding scheme. It requires
+ * the definition of the size of the padding block and the padding character.
+ * </p>
+ * 
+ * <p>If the padding is not enabled, the codec will add padding characters
+ * to align the encoded data to the padding block size. It will also threat
+ * the padding character as an invalid character while decoding the data.</p>
+ * 
+ * @author Fabio Jun Takada Chino
+ * @since 2015.03.11
+ */
 public class Base2NCodec extends AbstractCodec {
+	
+	/**
+	 * Constant that
+	 */
+	public static final char IGNORE_SPACES[] = " \t\n\r".toCharArray();
+	
+	public static final char IGNORE_NONE[] = new char[0];
 	
 	private final Alphabet alphabet;
 	private final int size;
@@ -11,26 +44,119 @@ public class Base2NCodec extends AbstractCodec {
 	private final int paddingBLockSize;
 	private final char ignored[];
 	
+	/**
+	 * Creates a new instance of this class. This constructor 
+	 * disables the padding and the ignore list.
+	 * 
+	 * @param alphabet The alphabet to be used. The size of the alphabet must be a power of two.
+	 */
+	public Base2NCodec(Alphabet alphabet) {
+		this(alphabet, 0, 0, null);
+	}
+
+	/**
+	 * Creates a new instance of this class. This constructor 
+	 * disables the ignore list.
+	 * 
+	 * @param alphabet The alphabet to be used. The size of the alphabet must be a power of two larger than zero 
+	 * and less than or equal to 128.
+	 * @param paddingChar The padding character.
+	 * @param paddingBlockSize The size of the padding block. Any value smaller than 2 disables the padding.
+	 */
+	public Base2NCodec(Alphabet alphabet, int paddingChar, int paddingBlockSize) {
+		this(alphabet, paddingChar, paddingBlockSize, null);
+	}
+
+	/**
+	 * Creates a new instance of this class.
+	 * 
+	 * @param alphabet The alphabet to be used. The size of the alphabet must be a power of two.
+	 * @param paddingChar The padding character.
+	 * @param paddingBlockSize The size of the padding block. Any value smaller than 2 disables the padding.
+	 * @param ignored List of characters to be ignored from the input. Use null to disable this feature.
+	 */
 	public Base2NCodec(Alphabet alphabet, int paddingChar, int paddingBlockSize, char ignored[]) {
 		
 		this.alphabet = alphabet;
-		this.paddingChar = paddingChar;
 		this.paddingBLockSize = paddingBlockSize;
-		this.size = alphabet.size();
+		if (paddingBLockSize != 0) {
+			this.paddingChar = paddingChar;
+		} else {
+			this.paddingChar = 0;
+		}
+		this.size = getCharacterSize(alphabet);
 		this.clearMask = (1 << this.size) - 1;
 		if (ignored != null) {
 			this.ignored = ignored.clone();
 		} else {
-			this.ignored = null;
+			this.ignored = IGNORE_NONE;
+		}
+	}
+	
+	private int getCharacterSize(Alphabet alphabet) throws IllegalArgumentException {
+		
+		switch (alphabet.size()) {
+		case 2:
+			return 1;
+		case 4:
+			return 2;
+		case 8:
+			return 3;
+		case 16:
+			return 4;
+		case 32:
+			return 5;
+		case 64:
+			return 6;
+		case 126:
+			return 7;
+		default:
+			throw new IllegalArgumentException("Invalid alphabet.");
 		}
 	}
 	
 	public int getDecodedSize(int encSize) {
 		return (encSize * size) / 8;
 	}
+	
+	/**
+	 * Returns the size of the padding.
+	 * 
+	 * @param totalSize The total size of the output.
+	 * @return The size of the padding. It is always zero if the padding is not used.
+	 */
+	protected int getPaddingSize(int totalSize) {
+		
+		if (usesPadding()) {
+			return (this.paddingBLockSize - (totalSize % this.paddingBLockSize)) % this.paddingBLockSize;
+		} else {
+			return 0;
+		}
+	}
 
 	public int getEncodedSize(int decSize) {
-		return (((decSize * 8) + size - 1) / size);
+		int totalSize;
+		
+		totalSize = (((decSize * 8) + size - 1) / size); 
+		return totalSize + getPaddingSize(totalSize);
+	}
+	
+	/**
+	 * Verifies if this instance uses padding or not.
+	 * 
+	 * @return true if the padding is used of false otherwise.
+	 */
+	public boolean usesPadding(){
+		return (this.paddingBLockSize >= 2);
+	}
+	
+	protected boolean isPadding(int c) {
+		
+		if (usesPadding()) {
+			return (c == this.paddingChar);
+		} else {
+			return false;
+		}
 	}
 	
 	/**
@@ -41,16 +167,14 @@ public class Base2NCodec extends AbstractCodec {
 	 */
 	protected boolean isIgnored(int c) {
 		
-		if (this.ignored != null) {
-			for (int i = 0; i < this.ignored.length; i++) {
-				if (c == this.ignored[i]) {
-					return true;
-				}
+		for (int i = 0; i < this.ignored.length; i++) {
+			if (c == this.ignored[i]) {
+				return true;
 			}
-		}		
+		}
 		return false;			
 	}
-	
+
 	public int decode(CharSequence src, int srcOffs, int srcSize, byte dst[], int dstOffs) {
 		
 		return this.decodeCore(src, srcOffs, srcSize, dst, dstOffs);
@@ -60,31 +184,71 @@ public class Base2NCodec extends AbstractCodec {
 		int bitBuffer;
 		int bitBufferSize;
 		int srcEndOffs;
-		int oldDstOffs;
 		int c;
+		int oldDstOffs;
+		boolean stop;
+		int paddingSize;
+		int srcTrueSize;
 		
+		// Scan the src
 		bitBuffer = 0;
 		bitBufferSize = 0;
 		oldDstOffs = dstOffs;
+		paddingSize = 0;
+		srcTrueSize = 0;
 		srcEndOffs = srcOffs + srcSize;
-		while (srcOffs < srcEndOffs) {
+		stop = srcOffs >= srcEndOffs;
+		while (!stop) {
 			// Get a character from source
 			c = src.charAt(srcOffs);
 			srcOffs++;
-
+			stop = srcOffs == srcEndOffs;
+			
+			// Check if the character must be ignored or not
 			if (!isIgnored(c)) {
-				// Add it to the bit buffer
-				bitBuffer = (bitBuffer << size) | (alphabet.getValue(c));
-				bitBufferSize += size;
-				
-				// Add bytes to dst
-				while (bitBufferSize >= 8) {
-					bitBufferSize -= 8;
-					dst[dstOffs] = (byte)((bitBuffer >> bitBufferSize) & 0xFF);
-					dstOffs++;
+				srcTrueSize++;
+				// Process the character.
+				if (isPadding(c)) {
+					stop = true;
+					paddingSize = 1;
+				} else {
+					// Add it to the bit buffer
+					bitBuffer = (bitBuffer << size) | (alphabet.getValue(c));
+					bitBufferSize += size;
+					
+					// Add bytes to dst
+					while (bitBufferSize >= 8) {
+						bitBufferSize -= 8;
+						dst[dstOffs] = (byte)((bitBuffer >> bitBufferSize) & 0xFF);
+						dstOffs++;
+					}
 				}
 			}
 		}
+		
+		// Check padding, if required
+		if (usesPadding()) {
+			if (paddingSize > 0) {
+				// Remove the padding
+				while (!stop) {
+					// Get a character from source
+					c = src.charAt(srcOffs);
+					srcOffs++;
+					stop = srcOffs == srcEndOffs;
+					// Verify if it has a valid padding or not
+					if (!isIgnored(c)) {
+						srcTrueSize++;
+						if (!isPadding(c)) {
+							throw new IllegalArgumentException("Invalid padding.");
+						}
+					}					
+				}
+			}
+			if ((srcTrueSize % this.paddingBLockSize) != 0) {
+				throw new IllegalArgumentException("The input is not properly padded.");
+			}
+		}
+
 		return  dstOffs - oldDstOffs;
 	}
 	
@@ -117,7 +281,15 @@ public class Base2NCodec extends AbstractCodec {
 			bitBuffer = bitBuffer << (size - bitBufferSize);
 			dst.append((char)this.alphabet.getCharacter(bitBuffer & clearMask));
 			dstSize++;
-		}		
+		}
+		
+		// Add padding if required
+		int paddingSize = getPaddingSize(dstSize);
+		dstSize += paddingSize;
+		while (paddingSize > 0) {
+			dst.append((char)this.paddingChar);
+			paddingSize--;
+		}
 		
 		return dstSize;
 	}	
