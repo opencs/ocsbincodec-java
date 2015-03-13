fffa7179ca737de08fbde0c7596267208e7809e1
Fabio Jun Takada Chino <fchino@opencs.com.br>
2015-03-13 08:46:45 -0300

    Documentation updated.

--- src/main/java/br/com/opencs/bincodec/HexadecimalAlphabet.java
+++ src/main/java/br/com/opencs/bincodec/HexadecimalAlphabet.java
@@ -1,17 +1,41 @@
 package br.com.opencs.bincodec;
 
+/**
+ * The hexadecimal alphabet.
+ * 
+ * @author Fabio Jun Takada Chino
+ * @since 2015.03.13
+ */
 public class HexadecimalAlphabet extends ArrayAlphabet {
 	
 	private static final String ALPHABET = "0123456789ABCDEF";
 
+	/**
+	 * Creates a new instance of this class. The method 
+	 * getCharacter() will always return upper case characters.
+	 */
 	public HexadecimalAlphabet() {
 		this(false);
 	}
 
-	public HexadecimalAlphabet(boolean lowercase) {
-		super(lowercase?ALPHABET.toLowerCase():ALPHABET);
+	/**
+	 * Creates a new instance of this class.
+	 * 
+	 * @param lowerCase Determines the behavior of getCharacter(). If true, it will
+	 * return lower case characters otherwise it will return upper case characters.
+	 */
+	public HexadecimalAlphabet(boolean lowerCase) {
+		super(lowerCase?ALPHABET.toLowerCase():ALPHABET);
 	}
 
+	/**
+	 * Returns the value of a given character. This method is case
+	 * insensitive.
+	 * 
+	 * @param c The character.
+	 * @return The value of the character. 
+	 * @throws IllegalArgumentException If c is not in the alphabet. 
+	 */
 	@Override
 	public int getValue(int c) throws IllegalArgumentException {
 
