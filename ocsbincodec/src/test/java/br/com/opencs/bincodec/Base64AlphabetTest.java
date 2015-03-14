5878a63f22128e90801d99f98972525892c708c2
Fabio Jun Takada Chino <fchino@opencs.com.br>
2015-03-13 23:48:27 -0300

    Base64Alphabet unit tests added along with the fixes detected by them.

--- ocsbincodec/src/test/java/br/com/opencs/bincodec/Base64AlphabetTest.java
+++ ocsbincodec/src/test/java/br/com/opencs/bincodec/Base64AlphabetTest.java
@@ -29,69 +29,73 @@
  */
 package br.com.opencs.bincodec;
 
-/**
- * The alphabet of Base64 as defined by the RFC4648. It can use the standard
- * and the URL safe variants.
- * 
- * @author Fabio Jun Takada Chino
- * @since 2015.03.13
- */
-public class Base64Alphabet extends ArrayAlphabet {
-	
-	private static final String ALPHABET_PREFIX = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"; 
-	private static final String STANDARD_POSTFIX = "+/";
-	private static final String SAFE_POSTFIX = "-_";
-	
-	private final int c62;
-	private final int c63;
-	
-	/**
-	 * Creates a new instance of this class using the standard alphabet.
-	 */
-	public Base64Alphabet(){
-		this(false);
-	}
-	
-	/**
-	 * Creates a new instance of this.
-	 * 
-	 * @param urlSafe If true, uses the URL safe alphabet otherwise used the standard one.
-	 */
-	public Base64Alphabet(boolean urlSafe){
-		super(ALPHABET_PREFIX + (urlSafe ? SAFE_POSTFIX : STANDARD_POSTFIX));
-		
-		if (urlSafe) {
-			c62 = '-';
-			c63 = '_';
-		} else {
-			c62 = '+';
-			c63 = '/';
-		}
-	}
-	
-	/**
-	 * Returns the value of a given character. This method is case
-	 * insensitive.
-	 * 
-	 * @param c The character.
-	 * @return The value of the character. 
-	 * @throws IllegalArgumentException If c is not in the alphabet. 
-	 */
-	@Override
-	public int getValue(int c) throws IllegalArgumentException {
+import static org.junit.Assert.*;
 
-		if ((c >= 'A') && (c <= 'Z')) {
-			return c - 'A';
-		} else if ((c >= 'a') && (c <= 'z')) {
-			return c - 'a' + 26;
-		} else  if ((c >= '0') && (c <= '9')) {
-			return c - '0' + 52;
-		} else if (c == c62) {
-			return 62;
-		} else if (c == c63) {
-			return 63;
-		} else {
-			throw new IllegalArgumentException("Invalid Base64 character.");
-		}
-	}	
+import org.junit.Test;
+
+public class Base64AlphabetTest extends BaseAlphabetTest {
+	
+	private static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+	private static final String ALPHABET_SAFE = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
+
+	@Test
+	public void testBase64Alphabet() {
+		Base64Alphabet a;
+		
+		a = new Base64Alphabet();
+		assertArrayEquals(ALPHABET.toCharArray(), a.alphabet);
+	}
+
+	@Test
+	public void testBase64AlphabetBoolean() {
+		Base64Alphabet a;
+		
+		a = new Base64Alphabet(false);
+		assertArrayEquals(ALPHABET.toCharArray(), a.alphabet);
+
+		a = new Base64Alphabet(true);
+		assertArrayEquals(ALPHABET_SAFE.toCharArray(), a.alphabet);
+	}
+	
+	@Test
+	public void testSize() {
+		Base64Alphabet a;
+		
+		a = new Base64Alphabet();
+		assertEquals(64, a.size());
+		
+		a = new Base64Alphabet(false);
+		assertEquals(64, a.size());
+
+		a = new Base64Alphabet(true);
+		assertEquals(64, a.size());
+	}
+	
+	@Test
+	public void testGetCharacter() {
+		Base64Alphabet a;
+		
+		a = new Base64Alphabet();
+		testGetCharacterCore(a, ALPHABET);
+		
+		a = new Base64Alphabet(false);
+		testGetCharacterCore(a, ALPHABET);
+
+		a = new Base64Alphabet(true);
+		testGetCharacterCore(a, ALPHABET_SAFE);	
+	}
+	
+	@Test
+	public void testGetValue() {
+		Base64Alphabet a;
+		
+		a = new Base64Alphabet();
+		testGetValueCore(a, ALPHABET);
+		
+		a = new Base64Alphabet(false);
+		testGetValueCore(a, ALPHABET);
+
+		a = new Base64Alphabet(true);
+		testGetValueCore(a, ALPHABET_SAFE);		
+	}
 }
